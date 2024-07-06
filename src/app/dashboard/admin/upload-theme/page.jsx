2d301fea90b92e076659473a989dca470bbe576d
Shahidul-Islam-BD <shahidulislam13sof@gmail.com>
2024-07-06 14:37:27 +0600

    value recieved from input field

--- src/app/dashboard/admin/upload-theme/page.jsx
+++ src/app/dashboard/admin/upload-theme/page.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+"use client"
+
+import React, { useState } from "react";
 
 const UploadTheme = () => {
+    const [description,setDescription] = useState('');
+
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        const form = e.target;
+        const name = form.name.value;
+        const version = form.version.value;
+        const price = form.price.value;
+        const thumbnails = form.thumbnails.value;
+        console.log(name,version,price,description);
+        console.log(thumbnails);
+
+    }
+
   return (
     <div>
       <h2 className="text-3xl font-bold mb-5">Upload your Theme</h2>
       <div className="border-2 border-gray-400 rounded-xl p-7">
-        <form>
+        <form onSubmit={handleSubmit}>
           <label className="form-control w-full">
             <div className="label">
               <span className="label-text text-lg font-medium">
@@ -16,6 +33,7 @@ const UploadTheme = () => {
               type="text"
               placeholder="Type project name"
               className="input input-bordered w-full max-w-xs"
+              name="name"
             />
           </label>
           <label className="form-control w-full">
@@ -28,6 +46,7 @@ const UploadTheme = () => {
               type="text"
               placeholder="Type project version"
               className="input input-bordered w-full max-w-xs"
+              name="version"
             />
           </label>
           <label className="form-control w-full">
@@ -40,6 +59,20 @@ const UploadTheme = () => {
               type="number"
               placeholder="Type project price"
               className="input input-bordered w-full max-w-xs"
+              name="price"
+            />
+          </label>
+          <label className="form-control w-full">
+            <div className="label">
+              <span className="label-text text-lg font-medium">
+                Project thumbnails
+              </span>
+            </div>
+            <input
+              type="file"
+              placeholder="import project thumbnails"
+              className="input input-bordered w-full max-w-xs"
+              name="thumbnails"
             />
           </label>
           <label className="form-control">
@@ -51,11 +84,15 @@ const UploadTheme = () => {
             <textarea
               className="textarea textarea-bordered h-24"
               placeholder="Write short description for your projects"
+              onChange={(e)=>setDescription(e.target.value)}
             ></textarea>
           </label>
-          <div className="flex justify-between mt-5">
-            <input  className="btn btn-sm btn-outline" type="submit" value="Submit" />
-            <button className="btn btn-sm btn-outline">Add new input field</button>
+          <div className="mt-5">
+            <input
+              className="btn btn-sm btn-outline"
+              type="submit"
+              value="Submit"
+            />
           </div>
         </form>
       </div>
